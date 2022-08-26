0a93a65989b2e522886139790e66e2ad5779c6b1
Lewis <lewismunyambu@gmail.com>
2022-08-26 21:35:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+ const submitData = (name,email)=>{
+return( fetch('http://localhost:3000/users',   {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": 'application/json'
+      
+    }, body: JSON.stringify({
+        name:name,
+        email:email
+      }),
+    }
+    
+  ).then(response=>response.json())
+  .then(json=> (console.log(json)))
+  .catch(error => document.body.innerHTML = error.message))}
+  console.log(submitData('lewis','lewis@gmail.com'))
+
+
+
+  
